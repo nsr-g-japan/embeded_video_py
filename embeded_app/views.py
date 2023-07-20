f47a82904bf845d5e541b53f3313729879db5bf9
nsr-g-japan <104160945+nsr-g-japan@users.noreply.github.com>
2023-07-20 17:03:44 +0530

    domian error

--- embeded_app/views.py
+++ embeded_app/views.py
@@ -7,18 +7,13 @@ import socket
 import getpass
 
 
-
-# Create your views here.
 def homepage(request):
-    # system_ip = ip.get()
-    #{'type': 'rtext', 'text': 'Public-IP:' + system_ip, 'alpha': '0.60', 'color': '0xFF0000', 'size': '15','interval': '5000'},
     hostname = socket.gethostname()
     IPAddr = socket.gethostbyname(hostname)
     print(socket.gethostbyaddr(IPAddr))
-    #{'type': 'rtext', 'text': 'User Name:' + hostname, 'alpha': '0.60', 'color': '0xFF0000', 'size': '15', 'interval': '5000'}
-    #{'type': 'rtext', 'text': 'Private-IP:' + IPAddr, 'alpha': '0.60', 'color': '0xFF0000', 'size': '15','interval': '5000'},
     username = getpass.getuser()
     print(username)
+
     url = "https://dev.vdocipher.com/api/videos/e7cc59d26da7440090daf9918d6d3b26/otp"
     water_mark = '\u00A9'
     water_mark += 'copyright by Firmbond K.K'
@@ -29,17 +24,12 @@ def homepage(request):
              'color': '0xFF0000', 'size': '12'},
             {'type': 'text', 'text': water_mark, 'alpha': '0.60', 'x': '10', 'y': '160',
              'color': '0xFF0000', 'size': '12'}
-
-
-
-
         ])
     })
     headers = {
         'Authorization': "Apisecret v68GeMNvpAxjwqaqZOMmF01g5TN2bupmjFge6SlindBqIXtw4quP6srUh5Y4M2Wy",
         'Content-Type': "application/json",
         'Accept': "application/json"
-
     }
     response = requests.request("POST", url, data=payload, headers=headers)
     json_object = json.loads(response.text)
